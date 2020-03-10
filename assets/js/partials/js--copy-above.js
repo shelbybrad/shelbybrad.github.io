eb7a9a7bc511669b3eed6d65d65be737dd625e0d
Brad Czerniak <ao5357@gmail.com>
2020-03-10 04:59:34 -0400

    Full RSVP form with radios

--- assets/js/partials/js--copy-above.js
+++ assets/js/partials/js--copy-above.js
@@ -6,25 +6,66 @@
  * Any id, for, name, etc. attributes are incremented numerically (and appended
  * if no number is there yet) so forms continue to work.
  *
+ * Due to the incrementing, ensure any form labels and aria relations are within
+ * the wrapper and at the same DOM nesting level.
+ *
  * Uses the DOM initializer pattern in partials/utility--initializer.js
  */
+
+const copyaboveAttrRegex = /^(.*)[:]{1,}([0-9]{1,})$/;
 
 var copyaboveInitializationFunction = function(initType) {
   this.classList.add('js--event--click');
 };
 utilityInitializer('js--copy-above', 'copyaboveInitializationFunction');
 
-/* Use event delegation for any dynamically-added dismiss events. */
+var copyaboveTraverse = function(elem, depth) {
+  depth = (typeof depth === 'number') ? depth+1 : 1;
+  const colons = new Array(depth + 1).join(':');
+
+  Array.prototype.forEach.call([
+    "aria-controls",
+    "aria-describedby",
+    "aria-details",
+    "aria-errormessage",
+    "aria-labelledby",
+    "aria-owns",
+    "for",
+    "id",
+    "itemid",
+    "name"
+  ], (thisAttr) => {
+    if (elem.hasAttribute(thisAttr)) {
+      const attrValue = elem.getAttribute(thisAttr);
+      const attrMatch = attrValue.match(copyaboveAttrRegex);
+      if (!attrMatch || !attrMatch[2]) {
+        elem.setAttribute(thisAttr, attrValue + colons + 2);
+      }
+      else {
+        elem.setAttribute(thisAttr, attrMatch[1] + colons
+          + (parseInt(attrMatch[2]) + 1));
+      }
+    }
+  });
+
+  if (elem.children.length) {
+    Array.prototype.forEach.call(elem.children, (childElement) => {
+      copyaboveTraverse(childElement, depth);
+    });
+  }
+};
+
+/* Use event delegation for any dynamically-added events. */
 document.addEventListener('click', function (event) {
   if (event.target !== document
     && event.target.closest('.js--copy-above')
   ) {
     let copyabove = event.target.closest('.js--copy-above');
-    let aboveElem = copyabove.previousElementSibling.cloneNode(true); /* Since it is a deep clone, we assume child elements are initiated. */
+    let aboveElem = copyabove.previousElementSibling.cloneNode(true);
     aboveElem.classList.add('js--dismissible');
 
-    /* Traverse the aboveElem and change unique attributes
-    * (original then a number of colons indicating depth then an elemnent-wide incrementer. */
+    /* Copy unique attributes and indicate depth with colons for template-inference. */
+    copyaboveTraverse(aboveElem);
 
     copyabove.insertAdjacentElement("beforeBegin", aboveElem);
   }
