16bfd6f16eae55d7896ab71ab051c75a9f983063
Brad Czerniak <ao5357@gmail.com>
2020-03-16 06:26:40 -0400

    Add pre-commit dependency option, for purgecss and terser

---
+++ .scripts/pre-commit.sh
@@ -0,0 +1,14 @@
+####
+## @file
+## Run pre-commit dependencies (minifying CSS and JavaScript files, mostly) and then do a quick jekyll build.
+##
+## This script requires that you install some node.js dependencies by running:
+##   `npm install`
+## from the repo root.
+##
+## Run this script from the repo root (ex: `.scripts/pre-commit.sh`).
+####
+
+npm run purgecss
+npm run terser
+bundle exec jekyll build --verbose
